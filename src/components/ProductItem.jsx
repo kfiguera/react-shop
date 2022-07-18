e2eef16688d07bcb15ccbdc10e119f4f0f9ff71a
Kevin Figuera <kevinalejandro3@gmail.com>
2022-07-17 22:52:02 -0400

    React.useState

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '@styles/ProductItem.scss';
 import btAddToCart from '@icons/bt_add_to_cart.svg';
+
 const ProductItem = () => {
-	return (
-		<div className="ProductItem">
-			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
-			<div className="product-info">
-				<div>
-					<p>$120,00</p>
-					<p>Bike</p>
-				</div>
-				<figure>
-					<img src={btAddToCart} alt="" />
-				</figure>
-			</div>
-		</div>
-	);
+	const [cart, setCart] = useState('');
+	const addToCart = () => {
+		setCart('hola mundo');
+	}
+    return (
+        <div className="ProductItem">
+            <img
+                src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                alt=""/>
+            <div className="product-info">
+                <div>
+                    <p>$120,00</p>
+                    <p>Bike</p>
+                </div>
+                <figure onClick={addToCart}>
+                    <img src={btAddToCart} alt=""/>
+                </figure>
+            </div>
+        </div>
+    );
 }
 
 export default ProductItem;
