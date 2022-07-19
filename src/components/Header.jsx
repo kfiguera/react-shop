ffa535ff824153e687127da1b30d9242c3d79eab
Kevin Figuera <kevinalejandro3@gmail.com>
2022-07-19 00:03:30 -0400

    Orden de compra

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,58 +1,64 @@
 import React, {useState, useContext} from 'react';
 import '@styles/Header.scss';
 import Menu from "@components/Menu";
+import MyOrder from "@containers/MyOrder";
 import iconMenu from '@icons/icon_menu.svg';
 import logoYardSale from '@logos/logo_yard_sale.svg';
 import iconShoppingCart from '@icons/icon_shopping_cart.svg';
 import AppContext from "@context/AppContext";
 
 const Header = () => {
-	const {state} = useContext(AppContext);
-	const [toggle, setToggle] = useState(false);
-	const handleToggle = () => {
-		setToggle(!toggle);
-	}
+    const {state} = useContext(AppContext);
+    const [toggle, setToggle] = useState(false);
+    const [toggleOrder, setToggleOrder] = useState(false);
+    const handleToggle = () => {
+        setToggle(!toggle);
+    }
 
-	return (
-		<nav>
-			<img src={iconMenu} alt="menu" className="menu" />
-			<div className="navbar-left">
-				<img src={logoYardSale} alt="logo" className="nav-logo" />
-				<ul>
-					<li>
-						<a href="/">All</a>
-					</li>
-					<li>
-						<a href="/">Clothes</a>
-					</li>
-					<li>
-						<a href="/">Electronics</a>
-					</li>
-					<li>
-						<a href="/">Furnitures</a>
-					</li>
-					<li>
-						<a href="/">Toys</a>
-					</li>
-					<li>
-						<a href="/">Others</a>
-					</li>
-				</ul>
-			</div>
-			<div className="navbar-right">
-				<ul>
-					<li className="navbar-email" onClick={handleToggle}>
-						platzi@example.com
-					</li>
-					<li className="navbar-shopping-cart">
-						<img src={iconShoppingCart} alt="shopping cart" />
-						{state.cart.length > 0 ? <div>{state.cart.length}</div>  : null}
-					</li>
-				</ul>
-			</div>
-			{ toggle && <Menu /> }
-		</nav>
-	);
+    return (
+        <nav>
+            <img src={iconMenu} alt="menu" className="menu"/>
+            <div className="navbar-left">
+                <img src={logoYardSale} alt="logo" className="nav-logo"/>
+                <ul>
+                    <li>
+                        <a href="/">All</a>
+                    </li>
+                    <li>
+                        <a href="/">Clothes</a>
+                    </li>
+                    <li>
+                        <a href="/">Electronics</a>
+                    </li>
+                    <li>
+                        <a href="/">Furnitures</a>
+                    </li>
+                    <li>
+                        <a href="/">Toys</a>
+                    </li>
+                    <li>
+                        <a href="/">Others</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="navbar-right">
+                <ul>
+                    <li className="navbar-email" onClick={handleToggle}>
+                        platzi@example.com
+                    </li>
+                    <li
+                        className="navbar-shopping-cart"
+                        onClick={() => {setToggleOrder(!toggleOrder)}}
+                    >
+                        <img src={iconShoppingCart} alt="shopping cart"/>
+                        {state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
+                    </li>
+                </ul>
+            </div>
+            {toggle && <Menu/>}
+            {toggleOrder && <MyOrder/>}
+        </nav>
+    );
 }
 
 export default Header;
--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -1,29 +1,35 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import OrderItem from '@components/OrderItem';
 import '@styles/MyOrder.scss';
 import flechita from '@icons/flechita.svg';
+import AppContext from "@context/AppContext";
 
 const MyOrder = () => {
-	return (
-		<aside className="MyOrder">
-			<div className="title-container">
-				<img src={flechita} alt="arrow" />
-				<p className="title">My order</p>
-			</div>
-			<div className="my-order-content">
-				<OrderItem />
-				<div className="order">
-					<p>
-						<span>Total</span>
-					</p>
-					<p>$560.00</p>
-				</div>
-				<button className="primary-button">
-					Checkout
-				</button>
-			</div>
-		</aside>
-	);
+    const {state} = useContext(AppContext);
+
+    return (
+        <aside className="MyOrder">
+            <div className="title-container">
+                <img src={flechita} alt="arrow"/>
+                <p className="title">My order</p>
+            </div>
+            <div className="my-order-content">
+                {state.cart.map((product) => (
+                    <OrderItem key={`orderItem-${product.id}`} product={product}/>
+                ))}
+
+                <div className="order">
+                    <p>
+                        <span>Total</span>
+                    </p>
+                    <p>$560.00</p>
+                </div>
+                <button className="primary-button">
+                    Checkout
+                </button>
+            </div>
+        </aside>
+    );
 }
 
 export default MyOrder;
